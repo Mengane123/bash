8ee208059240ca95951da8b4210473f06f07c737
prashant <prashantmengane007@gmail.com>
2024-08-16 21:32:54 +0530

    this script gives path of the file entered!

---
+++ give_path.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#taking input 
+read -p "Enter the file name : " file_name
+
+#finding the file 
+file_path= `find "/home/prashant" -name "$file_name"`
+#2>/dev/null will not display the errors/permission denied 
+
+##checking if file is found
+if [[ -z $file_path ]];then
+	echo "file '$file_name' does not exits"
+else
+	echo "File path(s) is : "
+	echo "$file_path"
+
+fi
+
